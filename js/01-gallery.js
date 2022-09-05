b34cd3edd615da702475ca1fc6b22fb3b1a42863
ssvyatoss <107925873+ssvyatoss@users.noreply.github.com>
2022-09-05 18:17:53 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryListEl = document.querySelector('.gallery');
+
+const makeGalleryCard = ({preview, original, description} = {}) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"  
+      />
+    </a>
+  </div>  
+`;
+};
+
+const galleryCardsArr = galleryItems.map(el => {
+    return makeGalleryCard(el);
+});
+
+galleryListEl.addEventListener('click', ev => {
+    ev.preventDefault();
+    if (ev.target.nodeName !== "IMG") {
+        return;
+    }
+    console.log(ev.target);
+    const instance = basicLightbox.create(`
+    <div class="modal">
+      <img src='${ev.target.dataset.source}'/>
+    </div>
+`)
+window.addEventListener('keydown', event => {
+    if (event .code === 'Escape') {
+        instance.close();
+    }
+    console.log(event);
+});
+
+
+instance.show();
+});
+
+
+
+galleryListEl.insertAdjacentHTML('afterbegin', galleryCardsArr.join(''))
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+const makeGalleryCard = ({original, preview, description} = {}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" 
+    src="${preview}"
+    alt="${description}"  
+    />
+  </a>
+`;
+};
+
+const galleryCardsArr = galleryItems.map(el => {
+    return makeGalleryCard(el);
+});
+
+galleryListEl.insertAdjacentHTML('afterbegin', galleryCardsArr.join(''))
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+console.log(SimpleLightbox);
